6d836a9302f6b87e75b6ab53bc7c243367763258
Ing. Maryan Espinoza <ANALISTA.GUMA@GMAIL.COM>
2023-10-27 10:03:13 -0600

    En Produccion

--- app/src/main/java/com/app/gmv3/activities/CardcreditActivity.java
+++ app/src/main/java/com/app/gmv3/activities/CardcreditActivity.java
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.app.gmv3.R;
-import com.craftman.cardform.Card;
-import com.craftman.cardform.CardForm;
-import com.craftman.cardform.OnPayBtnClickListner;
 
 public class CardcreditActivity extends AppCompatActivity {
 
@@ -14,13 +11,6 @@ public class CardcreditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardcredit);
-        CardForm cardForm = (CardForm) findViewById(R.id.card_form);
-        cardForm.setPayBtnClickListner(new OnPayBtnClickListner() {
-            @Override
-            public void onClick(Card card) {
-                //Your code here!! use card.getXXX() for get any card property
-                //for instance card.getName();
-            }
-        });
+
     }
 }
